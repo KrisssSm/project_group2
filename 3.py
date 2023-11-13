3e67f7bc1b6fb4a53eb96b61f2cc1bd059587d07
KrisssSm <146855749+KrisssSm@users.noreply.github.com>
2023-11-13 23:40:01 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,26 +1,23 @@
-а. n=int(input())
-k=int(input())
-print((n+k)/2)
+# Вводим целые числа n и k 
+n, k = map(int, input().split())
 
-b.n=int(input())
-k=int(input())
-print(sqrt(a*b))
-c. max = 0
-element = -1
-while element != 0:
-    element = int(input())
-    if element > max:
-        max = element
-print(max)
-d. first_max = int(input())
-second_max = int(input())
-if first_max < second_max:
-    first_max, second_max = second_max, first_max
-element = int(input())
-while element != 0:
-    if element > first_max:
-        second_max, first_max = first_max, element
-    elif element > second_max:
-        second_max = element
-    element = int(input())
-print(second_max)
+# Вводим n целых чисел, каждое с новой строки
+numbers = [int(input()) for _ in range(n)]
+
+# Для k первых элементов найдем и выведем:
+k_numbers = numbers[:k]  # выбираем только первые k элементов
+
+# Среднее арифметическое
+average_arithmetic = sum(k_numbers) / k
+
+# Среднее гармоническое
+average_harmonic = k / sum(1/x for x in k_numbers)
+
+# Максимальный элемент
+max_element = max(k_numbers)
+
+# Второй максимальный элемент
+second_max_element = sorted(k_numbers)[-2]
+
+# Минимальный элемент
+min_element = min(k_numbers)
